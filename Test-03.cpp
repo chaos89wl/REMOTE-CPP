1a1d69aeea24f0c81ed8e90c9f05dbe37b59a0e5
chaos89wl <chaos89@chollian.net>
2024-09-22 15:56:38 +0900

    sjna - Test-03.cpp 추가

---
+++ Test-03.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    // Open a file for writing
+    std::ofstream outputFile("output.txt");
+    if (!outputFile) {
+        std::cerr << "Failed to open output file." << std::endl;
+        return 1;
+    }
+
+    // 파일에 데이터 쓰기
+    outputFile << "Hello, World!" << std::endl;
+
+    // 출력 파일 닫기
+    outputFile.close();
+
+    // 파일을 읽기 모드로 열기
+    std::ifstream inputFile("output.txt");
+    if (!inputFile) {
+        std::cerr << "Failed to open input file." << std::endl;
+        return 1;
+    }
+
+    // 파일에서 데이터 읽기
+    std::string line;
+
+    while (std::getline(inputFile, line)) {
+        std::cout << line << std::endl;
+    }
+
+    // 입력 파일 닫기
+    inputFile.close();
+
+    return 0;
+}
